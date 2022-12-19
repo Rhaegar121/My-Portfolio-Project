79a98b069f1914e4db0eb1a167ea375013f80d94
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2022-12-19 19:01:17 +0630

    fix eslint error in js file

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const bar = document.querySelector(".fa-bars");
-const welcome = document.querySelector("#welcome");
-const menu = document.querySelector("#menu");
+const bar = document.querySelector('.fa-bars');
+const welcome = document.querySelector('#welcome');
+const menu = document.querySelector('#menu');
 
-bar.addEventListener("click", () => {
-  bar.classList.toggle("fa-xmark");
-  if (menu.style.display === "block" && welcome.style.display === "none") {
-    menu.style.display = "none";
-    welcome.style.display = "block";
+bar.addEventListener('click', () => {
+  bar.classList.toggle('fa-xmark');
+  if (menu.style.display === 'block' && welcome.style.display === 'none') {
+    menu.style.display = 'none';
+    welcome.style.display = 'block';
   } else {
-    menu.style.display = "block";
-    welcome.style.display = "none";
+    menu.style.display = 'block';
+    welcome.style.display = 'none';
   }
 });
 
-menu.addEventListener("click", () => {
-  bar.classList.toggle("fa-xmark");
-  if (menu.style.display === "block" && welcome.style.display === "none") {
-    menu.style.display = "none";
-    welcome.style.display = "block";
+menu.addEventListener('click', () => {
+  bar.classList.toggle('fa-xmark');
+  if (menu.style.display === 'block' && welcome.style.display === 'none') {
+    menu.style.display = 'none';
+    welcome.style.display = 'block';
   } else {
-    menu.style.display = "block";
-    welcome.style.display = "none";
+    menu.style.display = 'block';
+    welcome.style.display = 'none';
   }
 });
